c0c444260602f2888a617a880165b040604cbe38
Amadou Drammeh <drammehamadou>
2019-06-18 13:16:44 +0100

    Updated basic

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  appTitle: string = 'CAE Tech';
+  //appTitle: String = 'CAE Tech';
   constructor() { }
 
   ngOnInit() {
